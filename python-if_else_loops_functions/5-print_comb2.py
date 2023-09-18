b3438704a056fc8fc6b91bb6eacbfad895694d70
Adrian <dreamweaver3d@gmail.com>
2023-09-18 13:47:20 +0800

    pyif task 5 commit 2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    print('{0:02d}'.format(x), end="")
     if x != 99:
-        print(", ", end="")
+        print('{0:02d}, '.format(x), end="")
     else:
-        print("")
+        print('{0:02d}'.format(x))
