f92a46f8e56bc865e0c4b8f2c3403476dcc49a3a
Adrian <dreamweaver3d@gmail.com>
2023-09-18 13:12:05 +0800

    pyif task 1 commit 1

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number % 10
+
+if number < 0:
+    last = 10 - last
+
+front = f"Last digit of {number} is {last}"
+
+if last > 5:
+    print(f"{front} and is greater than 5")
+elif last < 6 and last != 0:
+    print(f"{front} and is less than 6 and not 0")
+else:
+    print(f"{front} and is zero")
