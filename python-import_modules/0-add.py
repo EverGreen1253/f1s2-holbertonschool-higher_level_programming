05a03cc18c7e731805a89188c8e75a69435e18d8
Adrian <dreamweaver3d@gmail.com>
2023-09-19 12:55:32 +0800

    pymod task 0 commit 3

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
+if __name__ == "__main__":
+    add = __import__('add_0').add
 
-a = 1
-b = 2
-c = add(a, b)
-print(f"{a} + {b} = {c}")
+    a = 1
+    b = 2
+    c = add(a, b)
+    print(f"{a} + {b} = {c}")
