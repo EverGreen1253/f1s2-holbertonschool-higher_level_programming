91cfaea6bb8b81223df65d37cfbf6e6f55ca0ff0
Adrian <dreamweaver3d@gmail.com>
2023-09-25 15:10:41 +0800

    pylist task 2 commit 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     """replaces the list element at the specified index."""
 
     if idx < 0 or idx >= len(my_list):
-        return None
+        return my_list
 
     my_list[idx] = element
     return my_list
