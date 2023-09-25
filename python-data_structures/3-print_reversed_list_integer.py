c3d5a7b78e5f13250c9c6a9dba987f85e61c1f88
Adrian <dreamweaver3d@gmail.com>
2023-09-25 15:16:34 +0800

    pylist task 3 commit 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """prints everything in the list in reverse."""
-    if mylist == None or len(my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return None
 
     i = len(my_list) - 1
