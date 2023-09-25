98275feee0c30b6cac5625b5796d43c1d3cedf7f
Adrian <dreamweaver3d@gmail.com>
2023-09-25 15:28:18 +0800

    pylist task 4 commit 3

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
-import copy
-
 def new_in_list(my_list, idx, element):
     """replaces the list element at the specified index."""
 
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = copy.copy(my_list)
+    new_list = my_list.copy()
 
     new_list[idx] = element
     return new_list
