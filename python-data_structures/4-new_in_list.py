575678afbddc5922aa106415dac770c87b857c47
Adrian <dreamweaver3d@gmail.com>
2023-09-25 15:22:03 +0800

    pylist task 4 commit 1

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import copy
+
+def replace_in_list(my_list, idx, element):
+    """replaces the list element at the specified index."""
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = copy.copy(my_list)
+
+    new_list[idx] = element
+    return new_list
