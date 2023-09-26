569f0493f19642e86afd441fdebb762a9169593f
Adrian <dreamweaver3d@gmail.com>
2023-09-26 09:50:23 +0800

    pylist task 10 commit 1

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """finds all multiples of 2 in list."""
+
+    if len(my_list) == 0:
+        return None
+
+    new_list = [None] * len(my_list)
+    i = 0
+    while i < len(my_list):
+        new_list[i] = my_list[i] % 2 == 0
+        i = i + 1
+
+    return new_list
+    
