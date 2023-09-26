4f5c8b7335c3aae5a227752d2dc4e720edfd026c
Adrian <dreamweaver3d@gmail.com>
2023-09-26 08:57:45 +0800

    pylist task 6 commit 1

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """removes c or C from the string."""
+
+    if len(matrix) == 0:
+        return None
+
+    i = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            print("{:d} ".format(matrix[i][j]), end="")
+            j = j + 1
+        print("")
+        i = i + 1
