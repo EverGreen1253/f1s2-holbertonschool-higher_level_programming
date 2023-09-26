25c47d24bba651c55d49046a51a43afb74deebf0
Adrian <dreamweaver3d@gmail.com>
2023-09-26 09:57:50 +0800

    pylist task 11 commit 2

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     """deletes item at specified index."""
 
-    if len(my_list) == 0 or idx < 0 or idx > len(my_list):
+    if len(my_list) == 0 or idx < 0 or idx > len(my_list) - 1:
         return my_list
 
     del my_list[idx]
