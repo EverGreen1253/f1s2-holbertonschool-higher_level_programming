44a8e550e43f6ad5cd5e83a25ecf1b1906108f63
Adrian <dreamweaver3d@gmail.com>
2023-09-26 17:15:06 +0800

    pydic task 9 commit 1

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """multiply dict items"""
+
+    new_dict = a_dictionary.copy()
+    for key, val in new_dict.items():
+        new_dict[key] = val * 2
+
+    return new_dict
