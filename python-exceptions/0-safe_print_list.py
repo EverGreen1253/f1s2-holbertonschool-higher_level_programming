9ac7c80feb269d0e589a77e5dc8386156919bdae
Adrian <dreamweaver3d@gmail.com>
2023-09-28 11:19:22 +0800

    pyerr task 0 commit 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,14 +2,20 @@
 def safe_print_list(my_list=[], x=0):
     """prints x elements of list"""
 
-    max = x
-    if x > len(my_list):
-        max = len(my_list)
+    str_len = 0
+    for i in my_list:
+        str_len = i
+
+    print(str_len)
+
+    max_len = x
+    if x > str_len:
+        max_len = str_len
 
     try:
-        for i in range(max):
+        for i in range(max_len):
             print("{0}".format(my_list[i]), end="")
         print("")
-        return max
+        return max_len
     except TypeError:
         return 0
