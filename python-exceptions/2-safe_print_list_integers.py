26901e7475c1759352e9116a04356fda8df5807e
Adrian <dreamweaver3d@gmail.com>
2023-09-28 13:56:34 +0800

    pyerr task 2 commit 1

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """prints x elements of list"""
+
+    str_len = 0
+    for i in my_list:
+        str_len = i
+
+    count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count = count + 1
+        print("")
+        return count
+    except IndexError:
+        print('list index out of range')
+        raise
