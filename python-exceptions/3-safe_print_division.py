9b3f5c38518a7cd02ddbe1f0f1f53bbcbbfff437
Adrian <dreamweaver3d@gmail.com>
2023-09-28 14:12:55 +0800

    pyerr task 3 commit 1

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """divides a by b"""
+    if b == 0:
+        print ("Inside result: None")
+        return None
+
+    result = a / b
+    print ("Inside result: {0}".format(result))
+    return result
