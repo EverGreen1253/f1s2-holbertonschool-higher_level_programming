0bd7285e110f88d7ea8ffa5c92d4836b150b6809
Adrian <dreamweaver3d@gmail.com>
2023-09-28 14:04:21 +0800

    pyerr task 2 commit 3

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,14 +9,10 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            if i >= str_len:
-                raise IndexError('list index out of range')
-
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count = count + 1
         print("")
         return count
     except IndexError:
-        print('list index out of range')
         raise
