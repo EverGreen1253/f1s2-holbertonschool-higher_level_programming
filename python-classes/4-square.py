083635be34700ca80a2392f1d939f056be8c0a81
Adrian <dreamweaver3d@gmail.com>
2023-10-05 13:45:46 +0800

    pyclasses task 4 commit 1

---
+++ python-classes/4-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Module for Square class """
+
+
+class Square:
+    """A square.
+
+    Args:
+        size (int): the square's size
+
+    Attributes:
+        size (int): the square's size
+
+    """
+
+    __size = 0
+
+    def __init__(self, size=0):
+        if isinstance(size, int) is not True:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Calculates area of square.
+
+            Returns:
+                int: calculated area of the square
+        """
+
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets size of square length.
+
+            Returns:
+                int: size of square length
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets size of square length.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(value, int) is not True:
+            raise TypeError('size must be an integer')
+
+        self.__size = value
