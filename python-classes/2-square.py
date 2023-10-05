567fe070e9eca97fcd1e49c981203700b3980e9d
Adrian <dreamweaver3d@gmail.com>
2023-10-05 13:03:53 +0800

    pyclasses task 2 commit 1

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for Square class """
+
+
+class Square:
+    """A square.
+
+    Args:
+        size (int): the square's size
+
+    Attributes:
+        size (int): the square's size
+
+    """
+
+    __size = 0
+
+    def __init__(self, size=0):
+        if isinstance(size, int) is not True:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
