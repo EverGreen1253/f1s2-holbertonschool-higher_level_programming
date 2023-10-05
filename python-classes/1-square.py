1b3292c59dd5e9ff3f1e4f43f011952d3ae9f70c
Adrian <dreamweaver3d@gmail.com>
2023-10-05 12:49:08 +0800

    gitclasses task 1 commit 3

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,26 +6,14 @@ class Square:
     """A square.
 
     Args:
-        length (int): the length of the square's size
+        size (int): the square's size
 
     Attributes:
-        length (int): the length of the square's size
-        area (int): the area of the square
+        size (int): the square's size
 
     """
 
-    # length = 0
     __size = 0
 
-    def __init__(self, length):
-        # self.length = length
-        self.__size = length * length
-
-    # def size(self):
-    #     """Example function with types documented in the docstring.
-
-    #         Returns:
-    #             int: calculated area of the square
-    #     """
-
-    #     return self.area
+    def __init__(self, size):
+        self.__size = size
