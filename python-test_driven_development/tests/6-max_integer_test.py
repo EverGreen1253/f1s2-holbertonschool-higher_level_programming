f36205371ac452a87a86063c92aa2ff7279cf0fd
Adrian <dreamweaver3d@gmail.com>
2023-10-06 16:13:09 +0800

    pytests task 5 commit 2

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,11 +6,17 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_max_ok(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_max_end(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
 
-    def test_min_ok(self):
-        self.assertNotEqual(max_integer([1, 2, 3, 4]), 1)
+    def test_max_start(self):
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+
+    def test_max_middle(self):
+        self.assertEqual(max_integer([1, 3, 2]), 3)
+
+    def test_negative_exists(self):
+        self.assertLessEqual(max_integer([-1, -2, -3]), 0)
 
     def test_empty(self):
         self.assertEqual(max_integer([]), None)
