3a226424d9cd6991a0b0ab19f95db293f2cb4a8a
Adrian <dreamweaver3d@gmail.com>
2023-10-06 15:06:50 +0800

    pytests task 3 commit 1

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains one function for printing a square with #
+"""
+
+
+def print_square(size):
+    """
+    Prints out the square.
+
+    Args:
+        size (int) : the size of one side
+
+    Returns:
+        Nothing.
+    """
+
+    if isinstance(size, int) is not True or (isinstance(size, float) is True and size < 0):
+        raise TypeError("size must be an integer")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print('#', end="")
+        print("")
