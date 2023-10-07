4038573b51c57781a44269917805cbeeb60f7f50
Adrian <dreamweaver3d@gmail.com>
2023-10-07 12:43:08 +0800

    pyclasses2 task 1 commit 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" Nameless module for Rectangle class """
+
+
+class Rectangle:
+    """A rectangle.
+
+    Args:
+        None
+
+    Returns:
+        Nothing
+
+    """
+
+    __width = 0
+    __height = 0
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Getter for private prop width
+
+            Returns:
+                value of __width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets value of width.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(self.__width, int) is not True:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for private prop height
+
+            Returns:
+                value of __height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets value of height.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(self.__height, int) is not True:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
