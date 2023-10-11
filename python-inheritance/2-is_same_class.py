f9db79462813dc3c4df297d4f1fb50b8a352d2ef
Adrian <dreamweaver3d@gmail.com>
2023-10-11 12:39:46 +0800

    pyinh task 2 commit 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,4 +5,4 @@
 def is_same_class(obj, a_class):
     """returns true / false depending on whether obj and class are the same."""
 
-    return isinstance(obj, a_class)
+    return type(obj) == a_class
