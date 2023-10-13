08358a89fdacd8fc2b0151389f171b3b9a9f8c14
Adrian <dreamweaver3d@gmail.com>
2023-10-13 13:15:33 +0800

    pyio task 5 commit 2

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -14,7 +14,5 @@ def save_to_json_file(my_obj, filename):
 
     """
 
-    data = json.dumps(my_obj)
-
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(data)
+        f.write(json.dumps(my_obj))
