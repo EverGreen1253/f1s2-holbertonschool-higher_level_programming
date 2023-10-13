1cd9e718f520ab49459b17e3f7c8df5c3b846535
Adrian <dreamweaver3d@gmail.com>
2023-10-13 11:14:54 +0800

    pyio task 0 commit 3

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 def read_file(filename=""):
     """Function docstring goes here"""
 
-    with open('my_file_0.txt', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end='')
     print("")
