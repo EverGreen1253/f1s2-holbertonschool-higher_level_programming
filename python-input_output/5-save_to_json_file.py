5583898d509179f5dd20f6b9c54181119ba4f515
Adrian <dreamweaver3d@gmail.com>
2023-10-13 13:10:10 +0800

    pyio task 5 commit 1

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module docstring goes here"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Convert to JSON string then write to file
+
+    Args:
+        my_obj (any): the data to convert to JSON
+
+    Returns:
+        Nothing
+
+    """
+
+    data = json.dumps(my_obj)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(data)
