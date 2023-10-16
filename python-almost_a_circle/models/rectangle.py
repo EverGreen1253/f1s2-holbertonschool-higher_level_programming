3c473403762899cdc0de686ce7b3cdb2808bc64b
Adrian <dreamweaver3d@gmail.com>
2023-10-16 11:53:48 +0800

    pycircle task 3 commit 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -49,8 +49,8 @@ class Rectangle(Base):
         if isinstance(value, int) is not True:
             raise TypeError("width must be an integer")
 
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        if value <= 0:
+            raise ValueError("width must be > 0")
 
         self.__width = value
 
