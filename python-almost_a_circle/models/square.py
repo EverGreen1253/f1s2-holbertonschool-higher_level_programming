83bda82a5ca982d12b6628da272f1c6ebade9009
Adrian <dreamweaver3d@gmail.com>
2023-10-16 13:03:49 +0800

    pycircle task 11 commit 1

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,3 +27,29 @@ class Square(Rectangle):
         """
         return ("[Square] ({0}) {1}/{2} - {3}"
                 .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """Getter for prop size
+
+            Returns:
+                value of size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets value of size.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(value, int) is not True:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
