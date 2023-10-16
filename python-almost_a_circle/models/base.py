d7737456464ab572487c95775eeac1c6027fbb62
Adrian <dreamweaver3d@gmail.com>
2023-10-16 11:01:59 +0800

    pycircle task 1 commit 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Nameless module for Base class """
+
+
+class Base:
+    """A Base class.
+
+    Args:
+        None
+
+    Returns:
+        Nothing
+
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
+
+    def __str__(self):
+        return self.id
