67376df0c2fb443f2c01452fbc652798390a1688
Adrian <dreamweaver3d@gmail.com>
2023-10-16 11:42:25 +0800

    pycircle task 2 commit 1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+""" Nameless module for Rectangle class """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A Rectangle class.
+
+    Args:
+        Base class it is inheriting from
+
+    Returns:
+        Nothing
+
+    """
+
+    __width = 0
+    __height = 0
+    __x = 0
+    __y = 0
+
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    def __str__(self):
+        return self.id
+
+    @property
+    def width(self):
+        """Getter for private prop width
+
+            Returns:
+                value of __width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets value of width.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(value, int) is not True:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for private prop height
+
+            Returns:
+                value of __height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets value of height.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(value, int) is not True:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for private prop x
+
+            Returns:
+                value of __x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets value of x.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(value, int) is not True:
+            raise TypeError("x must be an integer")
+
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for private prop y
+
+            Returns:
+                value of __y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets value of y.
+
+            Returns:
+                Nothing
+        """
+
+        if isinstance(value, int) is not True:
+            raise TypeError("y must be an integer")
+
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
