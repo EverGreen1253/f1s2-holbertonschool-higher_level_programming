b6bafd31d24a5253d5a90c734ac76a6442962dc2
Adrian <dreamweaver3d@gmail.com>
2023-10-17 14:29:13 +0800

    pycircle task 17 commit 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -69,3 +69,20 @@ class Base:
 
         with open(file_name, "w", encoding="utf-8") as f:
             f.write(str(to_save))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Convert string to json
+
+        Args:
+            json_string (str) : string representation of list of dicts
+
+        Returns:
+            list
+
+        """
+
+        if json_string is None or len(json_string) == 0:
+            return []
+
+        return json.loads(json_string)
