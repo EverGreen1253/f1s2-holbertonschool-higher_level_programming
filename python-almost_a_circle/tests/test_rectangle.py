9d521f157f19443640b36a4ed5203630bdff1965
Adrian <dreamweaver3d@gmail.com>
2023-10-19 14:12:09 +0800

    pycircle task 0 commit 10

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -103,7 +103,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.__str__(), "[Rectangle] (25) 0/0 - 2/5")
 
     def test_display_no_x_and_y_offset(self):
-        """Tests that the graphical representation prints out correctly
+        """Tests that the graphical representation prints out correctly with no offest
         """
         r = Rectangle(2, 2)
         capturedOutput = StringIO()     # Create StringIO object
@@ -113,7 +113,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(capturedOutput.getvalue(), "##\n##\n")
 
     def test_display_x_offset_only(self):
-        """Tests that the graphical representation prints out correctly
+        """Tests that the graphical representation prints out correctly with x offset
         """
         r = Rectangle(2, 2, 1)
         capturedOutput = StringIO()     # Create StringIO object
@@ -122,6 +122,83 @@ class TestRectangle(unittest.TestCase):
         sys.stdout = sys.__stdout__     # Reset redirect.
         self.assertEqual(capturedOutput.getvalue(), " ##\n ##\n")
 
+    def test_to_dictionary_method(self):
+        """Tests that the dictionary representation is correct
+        """
+        r = Rectangle(2, 2, 0, 0, 30)
+        d = r.to_dictionary()
+        self.assertEqual(d, {'x': 0, 'y': 0, 'id': 30, 'height': 2, 'width': 2})
+
+    def test_update_method_args(self):
+        """Tests that the update method works with args
+        """
+        r = Rectangle(2, 2, 1, 1)
+        r.update(99, 3, 3, 3, 3)
+        self.assertEqual(r.id, 99)
+        self.assertEqual(r.width, 3)
+        self.assertEqual(r.height, 3)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 3)
+
+    def test_update_method_kwargs(self):
+        """Tests that the update method works with kwargs
+        """
+        r = Rectangle(2, 2, 1, 1, 123)
+        r.update(**{'id': 321, 'width': 5, 'height': 5, 'x': 10, 'y': 10})
+        self.assertEqual(r.id, 321)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 5)
+        self.assertEqual(r.x, 10)
+        self.assertEqual(r.y, 10)
+
+    def test_parent_create_method_kwargs(self):
+        """Tests that the parent Base class' create method works with kwargs
+        """
+        r = Rectangle.create(**{'id': 123, 'width': 5, 'height': 5, 'x': 2, 'y': 2})
+        self.assertEqual(r.id, 123)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 5)
+        self.assertEqual(r.x, 2)
+        self.assertEqual(r.y, 2)
+
+    def test_parent_save_to_file_none(self):
+        """Tests empty input with parent Base class' save_to_file method
+        """
+        Rectangle.save_to_file(None)
+        filename = "Rectangle.json"
+
+        data = ""
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                data += line
+
+        self.assertEqual(data, "[]")
+
+    def test_parent_save_to_file_empty_list(self):
+        """Tests empty list input with parent Base class' save_to_file method
+        """
+        Rectangle.save_to_file([])
+        filename = "Rectangle.json"
+
+        data = ""
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                data += line
+
+        self.assertEqual(data, "[]")
+
+    def test_parent_save_to_file_list_rectangle(self):
+        """Tests list with Rectangle instantiator as input with parent Base class' save_to_file method
+        """
+        Rectangle.save_to_file([Rectangle(3,3,5,5,25)])
+        filename = "Rectangle.json"
+
+        data = ""
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                data += line
+
+        self.assertEqual(data, "[{\"x\": 5, \"y\": 5, \"id\": 25, \"height\": 3, \"width\": 3}]")
 
 if __name__ == '__main__':
     unittest.main()
