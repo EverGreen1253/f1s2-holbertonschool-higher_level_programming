ff3f051a739e9a59bf9c10352de4cc9f0942747c
Adrian <dreamweaver3d@gmail.com>
2023-10-20 11:18:39 +0800

    pycircle task 0 commit 14

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -100,6 +100,77 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s.x, 3)
         self.assertEqual(s.y, 3)
 
+    def test_update_method_kwargs(self):
+        """Tests that the update method works with kwargs
+        """
+        s = Square(2, 1, 1, 123)
+        s.update(**{'id': 321, 'size': 5, 'x': 10, 'y': 10})
+        self.assertEqual(s.id, 321)
+        self.assertEqual(s.size, 5)
+        self.assertEqual(s.x, 10)
+        self.assertEqual(s.y, 10)
+
+    def test_parent_create_method_kwargs(self):
+        """Tests that the parent Base class' create method works with kwargs
+        """
+        s = Square.create(**{'id': 123, 'size': 5, 'x': 2, 'y': 2})
+        self.assertEqual(s.id, 123)
+        self.assertEqual(s.size, 5)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 2)
+
+    def test_parent_save_to_file_none(self):
+        """Tests empty input with parent Base class' save_to_file method
+        """
+        Square.save_to_file(None)
+        filename = "Square.json"
+
+        data = ""
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                data += line
+
+        self.assertEqual(data, "[]")
+
+    def test_parent_save_to_file_empty_list(self):
+        """Tests empty list input with parent Base class' save_to_file method
+        """
+        Square.save_to_file([])
+        filename = "Square.json"
+
+        data = ""
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                data += line
+
+        self.assertEqual(data, "[]")
+
+    def test_parent_save_to_file_list_square(self):
+        """Tests list with Square instantiator as input with parent Base class' save_to_file method
+        """
+        Square.save_to_file([Square(3,5,5,25)])
+        filename = "Square.json"
+
+        data = ""
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                data += line
+
+        self.assertEqual(data, "[{\"id\": 25, \"x\": 5, \"size\": 3, \"y\": 5}]")
+
+    def test_parent_load_from_non_existant_file(self):
+        """Tests loading from a non-existant file with parent Base class' load_from_file method
+        """
+        l = Square.load_from_file()
+        self.assertEqual(l, [])
+
+    def test_parent_load_from_non_existing_file(self):
+        """Tests loading from an existing file with parent Base class' load_from_file method
+        """
+        Square.save_to_file(None)
+        l = Square.load_from_file()
+        self.assertEqual(str(l), "[]")
+
 
 
 if __name__ == '__main__':
