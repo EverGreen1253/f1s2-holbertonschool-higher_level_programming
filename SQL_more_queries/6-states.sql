e2f0a24ae0d0c6b163b5ca90eefb36c8cf9e37ad
Adrian <dreamweaver3d@gmail.com>
2023-11-08 13:21:29 +0800

    sql2 task 6 commit 2

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- creates a database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     `id` INT PRIMARY KEY NOT NULL DEFAULT 1,
     `name` VARCHAR(256) NOT NULL
