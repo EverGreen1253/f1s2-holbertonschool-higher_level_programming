5533b576d50a80c47cda9fab9777f910c3e1c00d
Adrian <dreamweaver3d@gmail.com>
2023-11-09 13:57:33 +0800

    sql2 task 15 commit 2

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -3,4 +3,5 @@ SELECT title
 FROM tv_genres g 
 INNER JOIN tv_show_genres sg ON g.id = sg.genre_id 
 INNER JOIN tv_shows s ON sg.show_id = s.id 
-WHERE name = "Comedy";
+WHERE name = "Comedy"
+ORDER BY title ASC;
