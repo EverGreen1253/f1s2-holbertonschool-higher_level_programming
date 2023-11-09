353071b0f9a1b11b782fdee02c8e3faa0df1e48e
Adrian <dreamweaver3d@gmail.com>
2023-11-09 14:01:42 +0800

    sql2 task 16 commit 1

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- list all shows and their linked genres
+SELECT title, name 
+FROM tv_shows s 
+LEFT JOIN tv_show_genres sg ON s.id = sg.show_id 
+LEFT JOIN tv_genres g ON sg.genre_id = g.id 
+ORDER BY s.title ASC, name ASC;
