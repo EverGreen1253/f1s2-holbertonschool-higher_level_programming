1eb750bda69b264cac561997cba77f9fbf196805
Adrian <dreamweaver3d@gmail.com>
2023-11-09 13:47:22 +0800

    sql2 task 14 commit 2

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- get the genres that Dexter is classified under
 SELECT DISTINCT (name) 
 FROM tv_shows s 
-INNER JOIN tv_show_genres sg ON s.id = sg.genre_id 
-INNER JOIN tv_genres g ON sg.genre_id = g.id
+LEFT JOIN tv_show_genres sg ON s.id = sg.genre_id 
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
 WHERE s.title = "Dexter";
