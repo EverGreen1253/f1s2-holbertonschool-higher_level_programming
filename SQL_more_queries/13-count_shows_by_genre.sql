b079084f9703c749c1908bf9434a7ac7c9ee02b6
Adrian <dreamweaver3d@gmail.com>
2023-11-09 13:34:59 +0800

    sql2 task 13 commit 2

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,13 @@
 -- list out the gneres and the number of shows that are tagged with them
-SELECT name, number_of_shows 
-FROM tv_genres LEFT JOIN (
-    SELECT genre_id AS genre, COUNT(show_id) AS number_of_shows
-    FROM tv_show_genres
-    GROUP BY genre_id
-) x 
-ON tv_genres.id = x.genre;
+SELECT name AS genre, COUNT(show_id) as number_of_shows 
+FROM tv_genres g 
+LEFT JOIN tv_show_genres sg ON g.id = sg.genre_id 
+GROUP BY name;
+
+-- SELECT name, number_of_shows 
+-- FROM tv_genres LEFT JOIN (
+--     SELECT genre_id AS genre, COUNT(show_id) AS number_of_shows
+--     FROM tv_show_genres
+--     GROUP BY genre_id
+-- ) x 
+-- ON tv_genres.id = x.genre;
