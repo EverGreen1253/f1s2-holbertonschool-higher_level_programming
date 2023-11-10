7a6d5473354b523892ff3de8b0efbd924f24a96d
Adrian <dreamweaver3d@gmail.com>
2023-11-10 11:25:05 +0800

    orm task 4 commit 3

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -26,4 +26,4 @@ if __name__ == "__main__":
     rows = c.fetchall()
 
     for row in rows:
-        print("({0}, '{1}')".format(row[0], row[1]))
+        print("({0}, '{1}', '{2}')".format(row[0], row[1], row[2]))
