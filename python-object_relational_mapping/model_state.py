22b708a2597f7aa1cc3e6f8a053dd8bfaa7247ce
Adrian <dreamweaver3d@gmail.com>
2023-11-10 13:35:26 +0800

    orm task 6 commit 1

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Start link class to table in database 
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
