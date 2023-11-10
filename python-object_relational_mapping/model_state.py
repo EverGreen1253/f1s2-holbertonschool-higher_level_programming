e9582ea0ca8a7fa4c7e1d123809da3c7953d36f1
Adrian <dreamweaver3d@gmail.com>
2023-11-10 13:38:14 +0800

    orm task 6 commit 3

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,11 +2,11 @@
 """Nameless module to declare tables for db hbtn_0e_6_usa
 """
 
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class State(Base):
     """Class to declare the states database table
