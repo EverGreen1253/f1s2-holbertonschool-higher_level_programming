a317fa3db91c6d34fbab23b801a58bedd90d047b
Adrian <dreamweaver3d@gmail.com>
2023-11-10 08:49:32 +0800

    orm task 0 commit 5

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,22 +2,23 @@
 """ Nameless module for running SQL """
 
 
-import MySQLdb
+if __name__ == "__main__":
+    import MySQLdb
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="",
-    database="hbtn_0d_usa"
-)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="",
+        database="hbtn_0d_usa"
+    )
 
-c = db.cursor()
-c.execute("""SELECT * FROM states""")
-rows = c.fetchall()
+    c = db.cursor()
+    c.execute("""SELECT * FROM states""")
+    rows = c.fetchall()
 
-for row in rows:
-    print("({0}, {1})".format(row[0], row[1]))
+    for row in rows:
+        print("({0}, {1})".format(row[0], row[1]))
 
-    # print("(%s, " % row[0], end = "")
-    # print("%s)" % row[1])
+        # print("(%s, " % row[0], end = "")
+        # print("%s)" % row[1])
