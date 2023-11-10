1161aa4d63041094f6925260a0c5fe77d112704a
Adrian <dreamweaver3d@gmail.com>
2023-11-10 08:33:52 +0800

    orm task 0 commit 1

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="",
+    database="hbtn_0d_usa"
+)
+
+c = db.cursor()
+c.execute("""SELECT * FROM states""")
+rows = c.fetchall()
+
+for row in rows:
+    print("({0}, {1})".format(row[0], row[1]))
+
+    # print("(%s, " % row[0], end = "")
+    # print("%s)" % row[1])
