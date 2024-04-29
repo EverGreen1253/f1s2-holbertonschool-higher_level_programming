6cf6716dbe9b6c2650f9bcf8fd34eeb52b71cca9
Adrian <dreamweaver3d@gmail.com>
2024-04-29 15:21:07 +0800

    task 2 solved

--- python-serialization/02-csv.py
+++ python-serialization/02-csv.py
@@ -7,7 +7,25 @@ import json
 def convert_csv_to_json(filename):
     """ does what it says """
 
-    # FIXME:
+    with open('data.csv', newline='') as csvfile:
+        read_data = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-    with open(filename, 'w') as f:
-        json.dump(data, f)
+        keys = []
+        data = []
+        row_index = 0
+        for row in read_data:
+            j = {}
+            col_index = 0
+            for i in row:
+                if row_index == 0:
+                    keys.append(i)
+                else:
+                    j[keys[col_index]] = i
+                col_index += 1
+            row_index += 1
+
+            if row_index > 1:
+                data.append(j)
+
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
