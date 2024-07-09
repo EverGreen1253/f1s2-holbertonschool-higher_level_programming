a0d952a96c1ff1cea8daec0340ce95e3046f3d23
Adrian <dreamweaver3d@gmail.com>
2024-07-09 10:32:52 +0800

    modified the code for task 0 to accomodate missing keys as well

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -22,8 +22,15 @@ def generate_invitations(template_content, attendees):
     for row in attendees:
         template_copy = template_content
 
-        for key, value in row.items():
+        for key in ["name", "event_title", "event_date", "event_location"]:
+            value = ""
             to_replace = "{" + key + "}"
+
+            # Not just the value! if the key is missing, we should also add 'N/A'!
+            if key not in row:
+                value = "N/A"
+            else:
+                value = row[key]
 
             if value == "" or value is None:
                 value = "N/A"
