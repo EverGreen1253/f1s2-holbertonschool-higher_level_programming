aeebb24a2a54c4e18583a757c840bf82ea03c26b
Adrian <dreamweaver3d@gmail.com>
2025-05-19 13:46:58 +0800

    Task 7 done

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let times = parseInt(process.argv[2])
+
+if (!isNaN(times)) {
+  if (times > 1) {
+    for (let i of Array(times).keys()) {
+        console.log('C is fun');
+    }
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
