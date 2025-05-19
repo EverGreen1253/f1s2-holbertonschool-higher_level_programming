349669de0829ea6550d56029c7522edbee792471
Adrian <dreamweaver3d@gmail.com>
2025-05-19 14:52:43 +0800

    Task 11 - removed extra console.log

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -8,7 +8,7 @@ if (process.argv.length <= 2) {
   console.log(0);
 } else {
   argList = process.argv.slice(2);
-  console.log(argList)
+  // console.log(argList)
 
   // Be careful, we're dealing with strings...
   for (const v of argList) {
