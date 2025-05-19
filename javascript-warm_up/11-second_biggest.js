91f7a4ad37a2b8a06c5c86c4273bfb869ecfb60f
Adrian <dreamweaver3d@gmail.com>
2025-05-19 14:51:05 +0800

    Task 11 fixed

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -8,9 +8,11 @@ if (process.argv.length <= 2) {
   console.log(0);
 } else {
   argList = process.argv.slice(2);
+  console.log(argList)
 
+  // Be careful, we're dealing with strings...
   for (const v of argList) {
-    if (v > largest) {
+    if (parseInt(v) > parseInt(largest)) {
       secondLargest = largest;
       largest = v;
     }
